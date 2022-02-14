a826fccb85851eeca113a9d013904c62f108abfb
Svetlozar <ext-svetlozar@getitdone.co>
2022-02-14 18:58:42 +0200

    Add pino configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      //forRoutes: [],
+      pinoHttp: {
+        formatters: {
+          level(level) {
+            return { level };
+          },
+        },
+        // level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  translateTime: true,
+                  colorize: true,
+                  ignore: 'pid,hostname',
+                  messageFormat: (log, messageKey) => {
+                    if ('req' in log) {
+                      return `[${log.context}] ${log[messageKey]} ${log.req.url}`;
+                    }
+                    return `[${log.context}] ${log[messageKey]}`;
+                  },
+                },
+              }
+            : undefined,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
